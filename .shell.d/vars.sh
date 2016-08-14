999d69a3f1e03b0ac9dfbf2e528165f3183fb470
Andrew Benson <adbenson@cmu.edu>
2016-08-13 22:07:15 -0700

    use termite as default terminal

--- .shell.d/vars.sh
+++ .shell.d/vars.sh
@@ -12,5 +12,5 @@ export PLAT
 
 export EDITOR=vim              # preferred editor is vim
 export DISPLAY=:0.0            # point X servers to localhost:0.0
-export TERMINAL=urxvt          # some programs use this as the default terminal
+export TERMINAL=termite        # some programs use this as the default terminal
 
