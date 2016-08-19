af071122a90b692326f638995065dca26b190e2c
Andrew Benson <adbenson@cmu.edu>
2016-08-19 15:05:45 -0700

    sort bash options

--- .shell.d/options.sh
+++ .shell.d/options.sh
@@ -3,9 +3,9 @@
 # shell options
 
 set -o notify       # When a job finishes, print the notification immediately
-shopt -s nocaseglob  # Use case-insensitive filename globbing
-shopt -s histappend  # Make bash append instead of overwrite the history on disk
-shopt -s cdspell     # Ignore small typos when changing directories
 shopt -s autocd      # Entering a directory name is interpreted as cd'ing to it
 shopt -s cdable_vars # cd interpretes non-directory names as shell variables
+shopt -s cdspell     # Ignore small typos when changing directories
+shopt -s histappend  # Make bash append instead of overwrite the history on disk
+shopt -s nocaseglob  # Use case-insensitive filename globbing
 
