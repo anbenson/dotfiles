c586f2082fdd95b059e3a4a4b180e4eb738011b1
Andrew Benson <adbenson@cmu.edu>
2016-09-04 13:53:51 -0400

    add file organization script

---
+++ .scripts/files.sh
@@ -0,0 +1,36 @@
+# files.sh
+# Andrew Benson
+# creates file hierarchy in $HOME directory
+
+# creates the given folder in $HOME, unless it already exists
+createhomefolder() {
+  if [ -d "$HOME/$1" ]; then
+    echo "[WARN] Tried to create $HOME/$1, but it already exists!"
+  else
+    mkdir -p "$HOME/$1"
+    echo "Created $HOME/$1."
+  fi
+}
+
+if [ $UID == 0 ]; then
+  echo "This script must be run as user!"
+  exit 1
+fi
+
+createhomefolder bin
+createhomefolder code
+createhomefolder doc
+createhomefolder games
+createhomefolder incoming
+createhomefolder life
+createhomefolder media
+createhomefolder media/music
+createhomefolder media/photos
+createhomefolder media/videos
+createhomefolder school
+createhomefolder temp
+createhomefolder transfer
+createhomefolder work
+
+echo -e "\nFinished."
+
