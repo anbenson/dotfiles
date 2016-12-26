1f658d7dce87c3dcf38dd932bfc072083c28299d
Andrew Benson <adbenson@cmu.edu>
2016-12-26 16:30:46 -0500

    fix typo in JAVA_HOME variable

--- .shell.d/vars.sh
+++ .shell.d/vars.sh
@@ -15,7 +15,7 @@ export BROWSER=firefox         # preferred browser is firefox
 export DISPLAY=:0.0            # point X servers to localhost:0.0
 export TERMINAL=termite        # some programs use this as the default terminal
 
-export JAVA_HOME=/usr/lib/jvm/java8-openjdk
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk
 export ANDROID_HOME=/opt/android-sdk
 
 export VIRTUAL_ENV_DISABLE_PROMPT=1 # disable default venv prompt modification
