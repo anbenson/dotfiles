7d64d50ebd04e85b6f2ca74077063022e2bd8816
Andrew Benson <adbenson@cmu.edu>
2017-02-04 03:03:09 -0500

    remove DISPLAY variable

--- .shell.d/vars.sh
+++ .shell.d/vars.sh
@@ -12,7 +12,6 @@ export PLAT
 
 export EDITOR=vim              # preferred editor is vim
 export BROWSER=firefox         # preferred browser is firefox
-export DISPLAY=:0.0            # point X servers to localhost:0.0
 export TERMINAL=termite        # some programs use this as the default terminal
 
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk
