7c977131d1e50ece1a916ab906bab07a9e1c2a88
Andrew Benson <adbenson@cmu.edu>
2017-04-02 04:35:02 -0400

    fix gtk-dark-firefox.py script

--- .scripts/gtk-dark-firefox.py
+++ .scripts/gtk-dark-firefox.py
@@ -4,7 +4,8 @@ firefoxdesktop = ""
 with open("/usr/share/applications/firefox.desktop", "r") as f:
   firefoxdesktop = f.read()
 
-firefoxdesktop = firefoxdesktop.replace("Exec=firefox", "Exec=env GTK_THEME=Arc-Darker firefox")
+firefoxdesktop = firefoxdesktop.replace("Exec=/usr/lib/firefox/firefox",
+        "Exec=env GTK_THEME=Arc-Darker /usr/lib/firefox/firefox")
 
 with open("/usr/share/applications/firefox.desktop", "w") as f:
   f.write(firefoxdesktop)
